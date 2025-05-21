b3e1f05a907a03d74606e967a60ec06a4f5593df
Andrei Lucas <andrei_rheivjarth@hotmail.com>
2025-05-21 19:50:49 -0300

    Domain model

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.entities.dto.GameMinDTO;
 import com.devsuperior.dslist.services.GameService;
 
